ffc886a36d567a0490b6f6dab83ac74424aa9e71
deflox <leo.rudin@gmx.ch>
2020-09-27 17:24:40 +0200

    Fix task 2 by adding floating point arrays and call the function from another file.

--- lab01/IT19tb_WIN7_S1_Aufg2.py
+++ lab01/IT19tb_WIN7_S1_Aufg2.py
@@ -1,5 +1,4 @@
 import numpy as np
-import matplotlib.pyplot as plt
 
 def fx(a, x):
     result = 0
@@ -8,44 +7,28 @@ def fx(a, x):
     return result
 
 def IT19tb_WIN7_S1_Aufg2(a, xmin, xmax):
-    #step = (xmax - xmin)/10.0
     x = np.arange(xmin, xmax, 1.0)
-    
-    p = np.arange(len(x))
+
+    # calculate y values for function
+    p = np.arange(len(x)*float(1))
     for i in range (0, len(x)):
         p[i] = fx(a, x[i])
     
-    # calculate differentiation coefficients
-    dp_cf = np.arange(len(a)-1)
+    # calculate differentiation coefficients and y values
+    dp_cf = np.arange((len(a)-1)*float(1))
     for i in range (0, len(a)-1):
         dp_cf[i] = (len(a)-i-1) * a[i]
     dp = np.arange(len(x))
     for i in range (0, len(x)):
         dp[i] = fx(dp_cf, x[i])
         
-    # calculate integral coefficients
-    pint_cf = np.arange(len(a) + 1)
-    pint_cf[len(pint_cf)-1] = 0;
+    # calculate integral coefficients and y values
+    pint_cf = np.arange((len(a) + 1)*float(1))
+    pint_cf[len(pint_cf)-1] = 0.0;
     for i in range (0, len(a)):
-        pint_cf[i] = (a[i]/(len(a)-i))*float(1)
+        pint_cf[i] = (a[i]/(len(a)-i))
     pint = np.arange(len(x))
     for i in range (0, len(x)):
         pint[i] = fx(pint_cf, x[i])
-        
-    print(pint_cf)
     
     return x,p,dp,pint
-
-input_a = [1,4,3]
-# x^2 + 2x + 3
-# 2x + 2
-# 1/3x^3 + x^2 + 3x + 0
-input_xmin = -10
-input_xmax = 10
-
-x_values, p_values, dp_values, pint_values = IT19tb_WIN7_S1_Aufg2(input_a, input_xmin, input_xmax)
-
-plt.plot(x_values,p_values)
-plt.plot(x_values,dp_values)
-plt.plot(x_values,pint_values)
-plt.grid()
---
+++ lab01/IT19tb_WIN7_S1_Aufg2_Script.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+from IT19tb_WIN7_S1_Aufg2 import IT19tb_WIN7_S1_Aufg2
+
+input_xmin = -10
+input_xmax = 10
+input_a = [1,2,3]
+
+x_values, p_values, dp_values, pint_values = IT19tb_WIN7_S1_Aufg2(input_a, input_xmin, input_xmax)
+
+plt.plot(x_values,p_values)
+plt.plot(x_values,dp_values)
+plt.plot(x_values,pint_values)
+plt.grid()
