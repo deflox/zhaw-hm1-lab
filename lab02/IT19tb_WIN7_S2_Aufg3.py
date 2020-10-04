5ea8cc9e701d5b77893f87fd72923b5a0293392c
stefanteodoropol <stefan.teodoropol@gmail.com>
2020-10-04 22:31:45 +0200

    Aufgabe 3 fertig

--- lab02/IT19tb_WIN7_S2_Aufg3.py
+++ lab02/IT19tb_WIN7_S2_Aufg3.py
@@ -2,39 +2,55 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Aufgabe 3a
-# Für grössere Werte wird pi genauer jedoch wird es danach wieder ungenauer.
+# Für grössere Werte wird 2*pi genauer jedoch wird es danach wieder ungenauer.
+# Ab 10^7 werden die Werte wieder ungenauer. Je mehr Eckpunkte auf dem Kreis
+# zur Annäherung erwendet werden, desto kleiner ist der Beitrag zum Umfang
+# je Dreieck. Wenn zu viele Eckpunkte betrachtet werden, wird die 
+# Seitenlänge der Dreiecke zu klein und zu ungenau, die resultierende
+# Summe wird ebenfalls ungenau.
+
 
 def s1(x):
-    if x < 6:
+    if x <= 6:
         return 1
     return np.sqrt(2.0-2.0*np.sqrt(1.0-((s1(x/2)**2)/4.0)))
 
-x_s1_values = np.zeros(22)
-y_s1_values = np.zeros(22)
+# Wir können vals ändern um kleinere oder grössere Wertebereiche zu prüfen
+vals = 25
+x_s1_values = np.zeros(vals)
+y_s1_values = np.zeros(vals)
 
-value = 12.0
-for i in range (0, 22):
+value = 6.0
+for i in range (0, vals):
     x_s1_values[i] = value
     y_s1_values[i] = value * s1(value)
     value = 2 * value
     
-plt.plot(x_s1_values, y_s1_values, color='red')
+plt.plot(x_s1_values, y_s1_values, color='red', label='Variante 3a)')
 
 # Aufgabe 3b
+# Bei der zweiten Variante beobachten wir, dass für grosse x-Werte die
+# resultierende Summe länger genau bleibt, bzw. weniger schnell vom erwarteten
+# Wert abweicht.
+# Wir vermuten, dass weil s_n^2 sowohl im Zähler als auch im Nenner auftaucht,
+# es zu einer kleinen Fehlerbereinigung kommt, und die Werte etwas länger
+# genau bleiben, und weniger stark abweichen als die Variante 3a).
+
 def s2(x):
-    if x < 6:
+    if x <= 6:
         return 1
     return np.sqrt( s1(x/2)**2.0 / (2 * (1 + np.sqrt(1 - ( (s1(x/2)**2)/4.0 )) ) ) )
 
-x_s2_values = np.zeros(22)
-y_s2_values = np.zeros(22)
+x_s2_values = np.zeros(vals)
+y_s2_values = np.zeros(vals)
 
-value = 12.0
-for i in range (0, 22):
+value = 6.0
+for i in range (0, vals):
     x_s2_values[i] = value
     y_s2_values[i] = value * s2(value)
     value = 2 * value
 
-plt.plot(x_s2_values, y_s2_values, color='blue')
-
+plt.plot(x_s2_values, y_s2_values, color='blue', label='Variante 3b)')
+plt.grid()
+plt.legend()
 plt.show()
