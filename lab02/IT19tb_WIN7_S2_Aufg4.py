e98bbc14c9fb71befb48ebe8ecb7e6917109d704
stefanteodoropol <stefan.teodoropol@gmail.com>
2020-10-04 22:58:35 +0200

    Aufgabe 4 Teil 1

--- lab02/IT19tb_WIN7_S2_Aufg4.py
+++ lab02/IT19tb_WIN7_S2_Aufg4.py
@@ -1,18 +1,57 @@
 # Aufgabe 4 Teil 1
+# Wir berechnen den kleinstmöglichen Wert von eps je nach der Basis 2 und 10.
+# Wir dividieren den Wert immer jeweils um die entsprechende Basis und prüfen
+# ob noch die Summe (1 + eps) noch verschieden von 1 ist.
+# Die Variante mit dem kleinsten eps am Schluss ist diejenige Variante
+# mit dem unser Rechner ebenfalls rechnet. Die andere Variante wird schneller
+# ungenau, weil die Basis noch umgerechnet werden muss.
+# Weiterhin schliessen wir auf ein Dualsystem, da der berechnete eps Wert mit 
+# der angenommene Basis 2 der gleiche Wert wie dem der IEEE double Precision
+# Gleitkommazahl entspricht. Diese ist in der Basis 2. 
 
-eps = 1.0
-while ((1+eps) != 1):
-    eps = eps/2
+print('Aufgabe 4 Teil 1:')
+eps_bin = 1.0
+stellen_bin = 0
+while ((1+eps_bin) != 1):
+    eps_bin = eps_bin/2
+    stellen_bin += 1
+
+eps_bin *= 2
+stellen_bin -= 1
     
-print(eps*2)
+print('eps_bin =',eps_bin)
+print('1 + eps_bin =', 1+eps_bin)
+print('stellen_bin =',stellen_bin)
+
+eps_dez = 1.0
+stellen_dez = 0
+while((1+eps_dez) != 1):
+    eps_dez = eps_dez/10
+    stellen_dez += 1
+    
+eps_dez *= 10
+stellen_dez -= 1    
+    
+print('eps_dez =',eps_dez)
+print('1 + eps_dez =', 1+eps_dez)
+print('stellen_dez =',stellen_dez)
+
+if (eps_dez < eps_bin):
+    print('Rechner rechnet mit Basis 10 genauer, es ist ein Dezimalsystem')
+
+if (eps_bin < eps_dez):
+    print('Rechner rechnet mit Basis 2 genauer, es ist ein Dualsystem')
+
+if (eps_bin == eps_dez):
+    print('Rechner rechnet sowohl mit Basis 2 als auch mit Basis 10 genau gleich präszise.')
+    print('Wir können anhand vom eps nicht bestimmen auf welche Basis gerechnet wird.')
+
 
 # Aufgabe 4 Teil 2
 
+print('Aufgabe 4 Teil 2:')
 maxx = 1.0
 while ((1.0+maxx)!=maxx):
     maxx=maxx*2
     
-while ((1.0+maxx) == maxx):
-    maxx=maxx-1
-    
-print(maxx+1)
+print(maxx)
